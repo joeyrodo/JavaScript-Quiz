dad9de511046a7bfc90a6a242c3486476215648b
joeyrodo <123434946+joeyrodo@users.noreply.github.com>
2023-03-27 22:01:09 -0400

    Added final changes to Quiz

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Module-4-Challenge
-This is a repository for my submission on the module 4 challenge in the Rutgers Coding Bootcamp.
+# Module 4 Challenge
+
+This is the README for the repository of the Module 4 Challenge of Joseph Rodante in Rutgers Coding Bootcamp. This application is a timed quiz on JavaScript that will award points to the player based on how many questions they answered correctly and on how much time is remaining. The player's score will be displayed at the bottom of the page once the quiz is completed.
+
+
+## Installation
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -7,38 +7,40 @@ var leaderboardScores = document.getElementById('leaderboard-scores');
 const startButton = document.getElementById('start-button');
 var timeLeft = 30;
 var currentScore = 0;
+var finalScore;
+var gameOverCalled = false;
 
-// Creating questions
-let question1JSON = '{ "question":"q1" , ' +
-    '"answer1":"q1a1", ' +
-    '"answer2":"q1a2", ' +
-    '"answer3":"q1a3", ' +
-    '"answer4":"q1a4", ' +
+    // Creating questions
+let question1JSON = '{ "question":"What is the correct way to write a JavaScript array?" , ' +
+    '"answer1": "var colors = 1 = (red), 2 = (green), 3 = (blue)", ' +
+    '"answer2": "var colors = [red, green, blue]", ' +
+    '"answer3": "var colors = (1:red, 2:green, 3:blue)", ' +
+    '"answer4": "var colors = red, green, blue", ' +
+    '"correct": "2"}';
+let question2JSON = '{ "question":"Which event occurs when the user clicks on an HTML element?" , ' +
+    '"answer1": "onmouseover", ' +
+    '"answer2": "onchange", ' +
+    '"answer3": "onclick", ' +
+    '"answer4": "onmouseclick", ' +
+    '"correct": "3"}';
+let question3JSON = '{ "question":"Which operator is used to assign a value to a variable?" , ' +
+    '"answer1": "=", ' +
+    '"answer2": "-", ' +
+    '"answer3": "*", ' +
+    '"answer4": "X", ' +
     '"correct": "1"}';
-let question2JSON = '{ "question":"q2" , ' +
-    '"answer1":"q2a1", ' +
-    '"answer2":"q2a2", ' +
-    '"answer3":"q2a3", ' +
-    '"answer4":"q2a4", ' +
-    '"correct": "1"}';
-let question3JSON = '{ "question":"q3" , ' +
-    '"answer1":"q3a1", ' +
-    '"answer2":"q3a2", ' +
-    '"answer3":"q3a3", ' +
-    '"answer4":"q3a4", ' +
-    '"correct": "1"}';
-let question4JSON = '{ "question":"q4" , ' +
-    '"answer1":"q4a1", ' +
-    '"answer2":"q4a2", ' +
-    '"answer3":"q4a3", ' +
-    '"answer4":"q4a4", ' +
-    '"correct": "1"}';
-let question5JSON = '{ "question":"q5" , ' +
-    '"answer1":"q5a1", ' +
-    '"answer2":"q5a2", ' +
-    '"answer3":"q5a3", ' +
-    '"answer4":"q5a4", ' +
-    '"correct": "1"}';
+let question4JSON = '{ "question":"How do you create a function in JavaScript?" , ' +
+    '"answer1": "function = myFunction()", ' +
+    '"answer2": "function:myFunction()", ' +
+    '"answer3": "function myFunction()", ' +
+    '"answer4": "myFunction.function()", ' +
+    '"correct": "3"}';
+let question5JSON = '{ "question":"How does a FOR loop start?" , ' +
+    '"answer1": "for (i = 0; i <=5)", ' +
+    '"answer2": "for i = 1 to 5", ' +
+    '"answer3": "for (i <= 5; i++)", ' +
+    '"answer4": "for (i = 0; i <= 5; i++)", ' +
+    '"correct": "4"}';
 
 // Shuffling the order of the questions
 let questions = [JSON.parse(question1JSON), JSON.parse(question2JSON), JSON.parse(question3JSON), JSON.parse(question4JSON), JSON.parse(question5JSON)]
@@ -61,7 +63,7 @@ function startGame() {
 }
 
 function showQuestion() {
-    if (currentQuestion == 5) {
+    if (currentQuestion == 5 && !gameOverCalled) {
         gameOver();
     }
     else {
@@ -120,32 +122,68 @@ function answerChosen4() {
 }
 
 function rightChosen() {
-    answers.children[0].textContent = "right answer chosen!";
     currentScore = currentScore + 10;
     score.textContent = "Score: " + currentScore;
     currentQuestion++;
     showQuestion();
 }
 function wrongChosen() {
-    answers.children[0].textContent = "wrong answer chosen!";
     timeLeft = timeLeft - 5;
     currentQuestion++;
     showQuestion();
 }
 
 function gameOver() {
+    gameOverCalled = true;
+    currentScore = currentScore + timeLeft;   
     timeLeft = 0;
-    questionText.textContent = "Game Over!";
+    finalScore = currentScore;
+    score.textContent = "Score: " + finalScore;
+    questionText.textContent = "Game Over! Refresh to play again!";
     timer.textContent = 'Timer: ' + 0;
+    leaderboard();
 }
 
 function showTime() {
+    if (timeLeft > 0) {
     timer.textContent = 'Timer: ' + timeLeft;
     timeLeft--;
-    if (timeLeft <= -1) {
+    }
+    else if (timeLeft <= 0 && !gameOverCalled)  {
         gameOver();
     }
 }
 
+function leaderboard() {
+    var counter = 0;
+    while (counter < leaderboardEntries.length) {
+    
+        if (finalScore < leaderboardEntries[counter].score) {
+            counter++;
+        }
+        
+        else {
+            leaderboardEntries[counter].name = prompt("Congradulations! You earned a high score! Please enter your name for the leaderboard!");
+            leaderboardEntries[counter].score = finalScore;
+            counter = leaderboardEntries.length;
+            displayScores();
+        }
+    
+    }
+}
 
+let entry1 = '{ "name":"" , ' + '"score":""}';
+let entry2 = '{ "name":"" , ' + '"score":""}';
+let entry3 = '{ "name":"" , ' + '"score":""}';
+let entry4 = '{ "name":"" , ' + '"score":""}';
+let entry5 = '{ "name":"" , ' + '"score":""}';
+
+let leaderboardEntries = [JSON.parse(entry1), JSON.parse(entry2), JSON.parse(entry3), JSON.parse(entry4), JSON.parse(entry5)]
+
+displayScores();
+
+function displayScores() {
+    for (i = 0; i < leaderboardEntries.length; i++)
+    leaderboardScores.children[i].textContent = leaderboardEntries[i].score + " - " + leaderboardEntries[i].name;
+}
 startButton.addEventListener("click", startGame);
